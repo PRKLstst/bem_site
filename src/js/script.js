179ed878d0a56611b4e1b11a8727b9e57f1e6ffb
Alexsandr <fifuwb@gmail.com>
2025-01-12 21:16:46 +0300

    fix carousel + js

--- src/js/script.js
+++ src/js/script.js
@@ -49,18 +49,15 @@ $(document).ready(function(){
     $('.carousel__inner').slick({
         speed: 1500,
         adaptiveHeight: true,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.svg"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.svg"></button>',
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
         responsive: [
             {
                 breakpoint: 992,
                 settings: {
-                dots: true,
                 arrows: false
                 }
-            }
+            },
         ]
       });
 });
